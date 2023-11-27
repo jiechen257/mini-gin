3cdef61123330d540e216447e446b3c769a77a94
jiechen <1487503910@qq.com>
2023-11-27 14:24:59 +0800

    feat: support middleware

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"log"
 	"mini-gin/gin"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.String(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := gin.New()
+	r.Use(gin.Logger())
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
@@ -22,18 +36,12 @@ func main() {
 		})
 	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *gin.Context) {
 			// expect /hello/ginktutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":9999")
